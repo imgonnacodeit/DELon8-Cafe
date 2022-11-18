1b126af29386774b09145bc8adea3b50690d3ca0
Kareem James <ciarrabalsomo@Ciarras-MacBook-Pro.local>
2022-11-18 13:13:56 +0000

    Functions for the products

--- Function/codetest.py
+++ Function/codetest.py
@@ -4,7 +4,7 @@ courierlist = []
 
 
 def createNew():
-    new_addition = input('Please enter the name')
+    new_addition = input('Please enter the name: ')
     new_addition = new_addition.lower()
     if new_addition == '':
         print('You cannot have an empty value.')
@@ -12,16 +12,45 @@ def createNew():
         return new_addition
 
 def PRODUCT_createPrice():
-    new_price = int(input('What is the price of the item? '))
+    new_price = int(input('What is the price of the item?: '))
     return new_price
-
 
 def PRODUCT_dic():
     diction = {'name': createNew(), 'price': PRODUCT_createPrice()}
     return diction
-    
-def PRODUCT_add_to_list():
-    productlist.append(PRODUCT_dic())
+
+def PRODUCT_add(x):
+    productlist.append(x)
+
+def PRODUCT_print():
+    index = 0
+    for product in productlist:
+        print(index, ': Product:', product['name'], 'Price:', product['price'])
+        index += 1
+
+def PRODUCT_update():
+    PRODUCT_print()
+    to_amend = int(input('Which item number would you like to change?: '))
+    name_or_price = int(input('Would you like to change. Select [1]Name or [2]Price: '))
+    if name_or_price == 1:
+        name_or_price = 'name'
+        amended_item = createNew()
+    elif name_or_price == 2:
+        name_or_price ='price'
+        amended_item = PRODUCT_createPrice()
+    productlist[to_amend][name_or_price] = amended_item            
+    PRODUCT_print()
+
+def PRODUCT_delete():
+    PRODUCT_print()
+    item_finder = int(input('Insert the item number that you would like to delete: '))
+    del productlist[item_finder]
+    print ('Your item has been deleted.')
+    PRODUCT_print()
+
+
+
+
 
 
 # def COURIER_createPhone_number():
@@ -31,6 +60,3 @@ def PRODUCT_add_to_list():
 # def COURIER_dic():
 #     dic = {'name': createNew(), 'phone number': COURIER_createPhone_number()}
 #     return dic
-
-# def COURIER_add_to_list():
-#     courierlist.append(COURIER_dic)
--- Function/project.py
+++ Function/project.py
@@ -1,22 +1,11 @@
 import codetest
-#Creating a new item
-def createNew():
-    new_addition = input('')
-    new_addition = new_addition.lower()
-    if new_addition == '':
-        print('You cannot have an empty value.')
-    else:
-        return new_addition
-    
-
-
 #LOAD products list from products.txt
-productsFile = open('products.txt', 'r')
-productsLine = productsFile.readlines()
-products = {}
-for p_line in productsLine:
-    p_line = p_line.strip()
-    products.append(p_line)
+# productsFile = open('products.txt', 'r')
+# productsLine = productsFile.readlines()
+# products = []
+# for p_line in productsLine:
+#     p_line = p_line.strip()
+#     products.append(p_line)
 
 
 #LOAD couriers list from couriers.txt
@@ -27,11 +16,10 @@ for c_line in courierLine:
     c_line = c_line.strip()
     courier.append(c_line)
 
-    #The above can be replace with a functions to not be repeating 
+#     The above can be replace with a functions to not be repeating 
 
 # An empty list to append the orders
 orders = []
-
 
 
 selection = 0
@@ -55,7 +43,7 @@ while selection == 0 :
     choice = (int(input('What is your choice: ')))
     if choice == 0:
         productsFile = open('products.txt', 'w')
-        productsFile.write('\n'.join(products))
+        productsFile.write('\n'.join(codetest.productlist))
         productsFile.close()
 
         courierFile = open('couriers.txt', 'w')
@@ -66,7 +54,7 @@ while selection == 0 :
         break
 
    
-    elif choice == 1:
+    if choice == 1:
         while True:
             print ('')
             print ('')
@@ -84,180 +72,26 @@ while selection == 0 :
             #Exit out of the product menu
             if product_menu_choice == 0:
                 break
-            
+
+
             #Takes everyproduct and prints it to a new line.
             if product_menu_choice == 1:
-                print(products)
-            
+                codetest.PRODUCT_print()
+
+
             #Creating new items in the list. 
             elif product_menu_choice == 2:
-                codetest.PRODUCT_add_to_list()
-                    
+                x = codetest.PRODUCT_dic()
+                codetest.PRODUCT_add(x)
+                
+
+
             #This updates the product list 
             elif product_menu_choice == 3:
+                codetest.PRODUCT_update()
 
-                item_number = 0
-                for i in products:
-                    print (item_number, ':', i) #change the name i to something else that is named better
-                    item_number = item_number + 1
-
-                to_amend = int(input('Which item number would you like to change?: '))
-                amended_item = input('What would you like to change this to?: ')
-
-                products[to_amend] = amended_item
-                
-                print(products)
 
 
             #This will delete something from the list using the index number.
             elif product_menu_choice == 4:
-                item_number = 0
-                for i in products:
-                    print (item_number, ':', i)
-                    item_number = item_number + 1
-                
-                item_finder = int(input('Insert the item number that you would like to delete: '))
-                del products[item_finder]
-                print ('Your item has been deleted.')
-                print (products)
-
-    elif choice == 3:
-        while True:
-            print ('')
-            print ('')
-            print ('COURIERS MENU')
-            print ('0. Return to the MAIN MENU.')
-            print ('1. To PRINT all couriers')
-            print ('2. To CREATE a new courier') 
-            print ('3. To UPDATE a courier')
-            print ('4. To DELETE a courier')
-            print('') 
-
-            #Taking an input for the products menu
-            courier_menu_choice = int(input(''))
-
-            #Exit out of the product menu
-            if courier_menu_choice == 0:
-                break
-            
-            #Takes everyproduct and prints it to a new line.
-            if courier_menu_choice == 1:
-                print(courier)
-
-            
-            #Creating new items in the list. 
-            elif courier_menu_choice == 2:
-                codetest.COURIER_add_to_list()
-                
-            #This updates the product list 
-            elif courier_menu_choice == 3:
-
-                courier_number = 0
-                for i in courier:
-                    print (courier_number, ':', i)
-                    courier_number = courier_number + 1
-
-                to_amend = int(input('Which item number would you like to change?: '))
-                amended_courier = input('What would you like to change this to?: ')
-
-                courier[to_amend] = amended_courier
-                
-                print(courier)
-
-
-            #This will delete something from the list using the index number.
-            elif courier_menu_choice == 4:
-                courier_number = 0
-                for i in courier:
-                    print (courier_number, ':', i)
-                    courier_number =  courier_number + 1
-                
-                courier_finder = int(input('Insert the item number that you would like to delete: '))
-                del courier[courier_finder]
-                print ('Your item has been deleted.')
-                print (courier)
-
-
-    if choice == 2:
-        while True:
-            print ('')
-            print ('')
-            print ('ORDERS MENU')
-            print ('0. Return to the MAIN MENU.')
-            print ('1. To PRINT all orders')
-            print ('2. To CREATE a new order') 
-            print ('3. To UPDATE STATUS')
-            print ('4. To UPDATE an order')
-            print ('5. To DELETE an order') 
-
-            orders_menu_choice = int(input(''))
-
-            #Return to previous menu
-            if orders_menu_choice == 0:
-                break
-            
-            #Prints all orders
-            elif orders_menu_choice == 1:
-                print(orders)
-
-            #Adding a new order
-            elif orders_menu_choice == 2:
-                order_name = input('Please enter the customer name: ')
-                order_address = (input('Enter the address number: '))
-                order_phone_number = input('Please enter the phone number: ')
-                order_status = 'PREPARING'
-                
-                single_order = {'Name': order_name, 'Address' : order_address, 'Phone Number' : order_phone_number, 'Status' : order_status}
-
-                orders.append(single_order)
-
-            #Updating existing orderstatus
-            elif orders_menu_choice == 3:
-                order_number = 0
-                for i in orders:
-                    print (order_number, ':', i)
-                    order_number = order_number + 1
-
-                to_amend = int(input('Which status would you like to change?: '))
-                
-                status_progression = ('PREPARING', 'READY')
-                progression = 0
-                for i in status_progression:
-                    print (progression, ':', status_progression[progression])
-                    progression = progression + 1
-
-                amended_order = int(input ('What would you like to change the status to?'))
-
-                orders[to_amend]['Status'] = status_progression[amended_order]
-                
-                print(orders)
-
-            #This is to update the existing orders
-            elif orders_menu_choice == 4:
-                order_number = 0
-                for i in orders:
-                    print (order_number, ':', i)
-                    order_number = order_number + 1
-                choice = int(input('Which order would you like to update? '))
-                print('You have chosen: ')
-                print(orders[choice])
-                second_choice = int(input('Which value would you like to change: '))
-                change = input('What would you like to change it to?: ')
-                orders[choice][second_choice] = change
-                
-                print(orders[choice])
-
-                
-            
-                
-                
-            
-            #This is deleting an entire order from the orders menu
-            elif orders_menu_choice == 5:
-                order_number = 0
-                for i in orders:
-                    print (order_number, ':', i)
-                    order_number = order_number + 1
-
-                deletingorder = int(input('Which order would you like to delete? '))
-                del orders[deletingorder]
+                codetest.PRODUCT_delete()
